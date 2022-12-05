f8005c6cd5f1b6afcbcabcfb9f5c5e1006760408
Shamus <mail@shamus.io>
2022-12-04 20:25:32 -0500

    Testing ADD

--- src/Quill.cs
+++ src/Quill.cs
@@ -4,13 +4,20 @@ namespace Quill
 {
   public class Quill
   {
+    private static readonly byte[] ROM = new byte[] { 0x3E, 0x0F, 0x87, 0x87, 0x87, 0x87, 0x87, 0x87, 0x87 };
+
     public static void Main(string[] args)
     {
       var cpu = new CPU();
-      while (true)
+      var cycles = 0;
+
+      cpu.LoadProgram(ROM);
+
+      while (cycles < ROM.Count())
       {
-        Console.WriteLine(cpu.ToString());
         cpu.Step();
+        Console.WriteLine(cpu.ToString());
+        cycles++;
       }
     }
   }
