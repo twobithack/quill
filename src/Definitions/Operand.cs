4d7032155cb152364efcef24be83730dd6f7ea0a
twobithack <4063261+twobithack@users.noreply.github.com>
2024-09-20 17:27:04 -0700

    Finish defining opcodes

--- src/Definitions/Operand.cs
+++ src/Definitions/Operand.cs
@@ -2,22 +2,81 @@ namespace Sonic.Definitions
 {
   public enum Operand 
   {
-    None,
-    WordImmediate,
-    A,
-    B,
-    C,
-    D,
+    Implied,
+    Immediate,
+    Indirect,
+    Relative,
+
+    // 8-bit registers
+    A, 
+    B, 
+    C, 
+    D, 
     E,
-    F,
+    F, 
+    H, 
     L,
-    HL,
-    AF,
-    BC,
+
+    // 16-bit registers
+    AF, 
+    BC, 
     DE, 
-    IX,
-    IY,
+    HL, 
+    IX,  
+    IY, 
     PC,
-    SP
+    SP,
+
+    // register indirect
+    BCi,
+    DEi,
+    HLi,
+    IXi,
+    IYi,
+
+    // displacement index
+    IXd,
+    IYd,
+
+    // flags
+    Zero, 
+    NonZero,
+    Carry, 
+    NonCarry,
+    Even,
+    Odd,
+    Negative,
+    Positive,
+
+    // shadow registers
+    Ap,
+    Bp,
+    Cp,
+    Dp,
+    Ep,
+    Fp,
+    AFp,
+    BCp, 
+    DEp,
+
+    // bit indexes
+    Bit0,
+    Bit1,
+    Bit2,
+    Bit3,
+    Bit4,
+    Bit5,
+    Bit6,
+    Bit7,
+
+    // restart addresses
+    RST0,
+    RST1,
+    RST2,
+    RST3,
+    RST4,
+    RST5,
+    RST6,
+    RST7
   } 
 }
---
+++ src/Instructions.cs
@@ -0,0 +1,303 @@
+using static Sonic.Definitions.Opcodes;
+
+namespace Sonic
+{
+  public partial class CPU
+  {
+    private void ADC()
+    {
+
+    }
+
+    private void ADD()
+    {
+
+    }
+
+    private void AND()
+    {
+
+    }
+    
+    private void BIT()
+    {
+      
+    }
+
+    
+    private void CALL()
+    {
+      
+    }
+
+    private void CCF()
+    {
+
+    }
+
+    private void CP()
+    {
+      
+    }
+
+    private void CPD()
+    {
+      
+    }
+
+    private void CPI()
+    {
+      
+    }
+
+    private void CPIR()
+    {
+      
+    }
+
+    private void CPL()
+    {
+      
+    }
+
+    private void DAA()
+    {
+      
+    }
+
+    private void DEC()
+    {
+      
+    }
+
+    private void DI()
+    {
+      
+    }
+
+    private void DJNZ()
+    {
+      
+    }
+
+    private void EI()
+    {
+      
+    }
+
+    private void EX()
+    {
+      
+    }
+
+    private void EXX()
+    {
+      
+    }
+
+    private void EN()
+    {
+      
+    }
+ 
+    private void HALT()
+    {
+      
+    }
+
+    private void IM()
+    {
+      
+    }
+ 
+    private void IN()
+    {
+      
+    }
+
+    private void INC()
+    {
+      
+    }
+
+    private void IND()
+    {
+      
+    }
+
+    private void INI()
+    {
+      
+    }
+
+    private void INIR()
+    {
+      
+    }
+
+    private void JP()
+    {
+      
+    }
+
+    private void JR()
+    {
+      
+    }
+
+    private void LD()
+    {
+      
+    }
+
+    private void NEG()
+    {
+      
+    }
+
+    private void NOP()
+    {
+      
+    }
+
+    private void OR()
+    {
+      
+    }
+
+    private void OTDR()
+    {
+      
+    }
+
+    private void OTIR()
+    {
+      
+    }
+
+    private void OUT()
+    {
+      
+    }
+
+    private void OUTD()
+    {
+      
+    }
+
+    private void OUTI()
+    {
+      
+    }
+
+    private void POP()
+    {
+      
+    }
+
+    private void PUSH()
+    {
+      
+    }
+
+    private void RES()
+    {
+      
+    }
+
+    private void RL()
+    {
+      
+    }
+
+    private void RLA()
+    {
+      
+    }
+
+    private void RLC()
+    {
+      
+    }
+
+    private void RLCA()
+    {
+      
+    }
+
+    private void RLD()
+    {
+      
+    }
+
+    private void RR()
+    {
+      
+    }
+
+    private void RRA()
+    {
+      
+    }
+
+    private void RRC()
+    {
+      
+    }
+
+    private void RRCA()
+    {
+      
+    }
+
+    private void RRD()
+    {
+      
+    }
+
+    private void RST()
+    {
+      
+    }
+
+    private void SBC()
+    {
+      
+    }
+
+    private void SCF()
+    {
+
+    }
+
+    private void SET()
+    {
+
+    }
+
+    private void SLA()
+    {
+
+    }
+
+    private void SRA()
+    {
+
+    }
+
+    private void SRL()
+    {
+
+    }
+
+    private void SUB()
+    {
+
+    }
+
+    private void XOR()
+    {
+
+    }
+  }
+}
