f4a2631d3b4973be0e97cb5b1706bfee8d4a127a
twobithack <4063261+twobithack@users.noreply.github.com>
2024-09-20 17:27:06 -0700

    Add border cropping options

--- src/Core/Configuration.cs
+++ src/Core/Configuration.cs
@@ -2,6 +2,8 @@ namespace Quill.Core;
 
 public class Configuration
 {
-  public int ScaleFactor { get; set; }
-  public int ExtraScanlines { get; set; }
+  public bool CropBottomBorder { get; set; } = true;
+  public bool CropLeftBorder { get; set; } = true;
+  public int ExtraScanlines { get; set; } = 0;
+  public int ScaleFactor { get; set; } = 1;
 }
