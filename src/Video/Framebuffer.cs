30dec6df1b850872a2bd98353b81f1953b072a87
twobithack <4063261+twobithack@users.noreply.github.com>
2025-10-26 21:16:03 -0700

    Implement triple buffering

--- src/Video/Framebuffer.cs
+++ src/Video/Framebuffer.cs
@@ -14,18 +14,19 @@ unsafe public sealed class Framebuffer
   #endregion
 
   #region Fields
-  private readonly byte[] _frontBuffer;
+  private bool _frontBufferToggle;
+  private readonly byte[] _frontBufferA;
+  private readonly byte[] _frontBufferB;
   private readonly int[] _backBuffer;
   private readonly bool[] _occupied;
-  private readonly Lock _bufferLock;
   #endregion
 
   public Framebuffer()
   {
-    _frontBuffer = new byte[FRONT_BUFFER_SIZE];
+    _frontBufferA = new byte[FRONT_BUFFER_SIZE];
+    _frontBufferB = new byte[FRONT_BUFFER_SIZE];
     _backBuffer = new int[BACK_BUFFER_SIZE];
     _occupied = new bool[BACK_BUFFER_SIZE];
-    _bufferLock = new Lock();
   }
 
   #region Methods
@@ -43,19 +44,22 @@ unsafe public sealed class Framebuffer
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   public void PushFrame()
   {
-    lock (_bufferLock)
-      Buffer.BlockCopy(_backBuffer, 0, _frontBuffer, 0, FRONT_BUFFER_SIZE);
+    var bufferToggle = Volatile.Read(ref _frontBufferToggle);
+    var frontBuffer = bufferToggle
+                    ? _frontBufferA
+                    : _frontBufferB;
+
+    Buffer.BlockCopy(_backBuffer, 0, frontBuffer, 0, FRONT_BUFFER_SIZE);
+    Volatile.Write(ref _frontBufferToggle, !bufferToggle);
 
     Array.Clear(_backBuffer);
     Array.Clear(_occupied);
   }
 
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
-  public byte[] PopFrame()
-  {
-    lock (_bufferLock)
-      return _frontBuffer;
-  }
+  public byte[] PopFrame() => !Volatile.Read(ref _frontBufferToggle) 
+                            ? _frontBufferA 
+                            : _frontBufferB;
 
   [MethodImpl(MethodImplOptions.AggressiveInlining)]
   private static int GetPixelIndex(int x, int y) => x + (y * FRAME_WIDTH);
