239867ee05c20b995b7df1335225f90a3bd8ad6f
twobithack <4063261+twobithack@users.noreply.github.com>
2025-11-08 19:13:08 -0700

    Cleaning up

--- src/Sound/PSG.cs
+++ src/Sound/PSG.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 using Quill.Common.Extensions;
 using Quill.Common.Interfaces;
@@ -8,6 +7,16 @@ namespace Quill.Sound;
 
 public sealed partial class PSG
 {
+  #region Constants
+  public const int CYCLES_PER_SAMPLE = 16;
+  public const int CHANNEL_COUNT = 4;
+
+  private const int TONE0 = 0b_00;
+  private const int TONE1 = 0b_01;
+  private const int TONE2 = 0b_10;
+  private const int NOISE = 0b_11;
+  #endregion
+
   public PSG(IAudioSink audioSink)
   {
     _audioSink = audioSink;
